b2f39e70652529d0fdceeafe906709478ade8945
Yaroslav <sjavao@gmail.com>
2021-04-28 00:31:36 +0300

    final

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     slug = models.SlugField(unique=True)
 
@@ -14,7 +14,6 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    objects = []
     text = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,

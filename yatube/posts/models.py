c13f058ed5845971a9b37f16563e9cb9a34739f8
Syomkin Denis <d5795@yandex.ru>
2021-04-27 23:57:05 +0300

    fix string langth

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -17,5 +17,7 @@ class Post(models.Model):
     objects = []
     text = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='posts', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='posts')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE,
+                              related_name='posts', blank=True, null=True)

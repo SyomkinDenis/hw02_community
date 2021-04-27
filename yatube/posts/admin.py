21d14e4ac9763d77e2b5bd4379045bff7748b8e6
Yaroslav <sjavao@gmail.com>
2021-04-27 23:05:00 +0300

    some changes

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые должны отображаться в админке
-    list_display = ("text", "pub_date", "author")
+    list_display = ('text', 'pub_date', 'author')
     # добавляем интерфейс для поиска по тексту постов
-    search_fields = ("text",)
+    search_fields = ('text',)
     # добавляем возможность фильтрации по дате
-    list_filter = ("pub_date",)
+    list_filter = ('pub_date',)
     # это свойство сработает для всех колонок: где пусто - там будет эта строка
-    empty_value_display = "-пусто-"
-    #prepopulated_fields = {"slug": ("title", "id")}
+    empty_value_display = '-пусто-'
 
 
-# при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+
+
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+from .models import Post, Group
+
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
-    return render(request, "index.html", {"posts": latest})
+    latest = Post.objects.order_by('-pub_date')[:12]
+    return render(request, 'index.html', {'posts': latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    # posts = group.posts.order_by('-pub_date')[:12]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:12]
+    render(request, 'group.html', {'group': group, 'posts': posts})
